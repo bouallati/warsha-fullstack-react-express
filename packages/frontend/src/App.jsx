efd6a3fc84006703146bd4b1e860ba9fb4266b2f
bouallati <bouallati.info@gmail.com>
2026-01-06 00:53:31 +0100

    feat(frontend): initialize react app with vite and tailwind css v4

--- packages/frontend/src/App.jsx
+++ packages/frontend/src/App.jsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <div>
+      <h1 className="text-red-500 underline ">warsha-fullstack-react-express</h1>
+    </div>
+  );
 }
 
 export default App;
